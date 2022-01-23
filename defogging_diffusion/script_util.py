3c5b3f89337633d9fc459f596c086b42db4d1933
RyanCaesarRamos <60872757+RyanCaesarRamos@users.noreply.github.com>
2022-01-23 14:02:09 +0800

    Add model_and_diffusion_defaults()

--- defogging_diffusion/script_util.py
+++ defogging_diffusion/script_util.py
@@ -24,6 +24,31 @@ def diffusion_defaults():
     )
 
 
+def model_and_diffusion_defaults():
+    """
+    Defaults for image training.
+    """
+    res = dict(
+        image_size=64,
+        num_channels=128,
+        num_res_blocks=2,
+        num_heads=4,
+        num_heads_upsample=-1,
+        num_head_channels=-1,
+        attention_resolutions="16,8",
+        channel_mult="",
+        dropout=0.0,
+        class_cond=False,
+        use_checkpoint=False,
+        use_scale_shift_norm=True,
+        resblock_updown=False,
+        use_fp16=False,
+        use_new_attention_order=False,
+    )
+    res.update(diffusion_defaults())
+    return res
+
+
 def df_model_and_diffusion_defaults():
     res = model_and_diffusion_defaults()
     res["image_size"] = 256
