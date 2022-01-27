13a558f557f8b2e3d609f8339424a1c235f8da2c
RyanCaesarRamos <60872757+RyanCaesarRamos@users.noreply.github.com>
2022-01-27 09:56:21 +0800

    Fix setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="defogging-diffusion",
-    py_modules=["defogging_diffusion"],
+    packages=["defogging_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm", "einops", "torchvision", "h5py"],
 )
